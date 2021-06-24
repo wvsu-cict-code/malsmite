882bfdd76b163f0ddb57b13fd1ba428a89606804
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-06-24 15:47:20 +0800

    Added mascot textures

--- MalSmite/player/Player.gd
+++ MalSmite/player/Player.gd
@@ -10,6 +10,7 @@ var velocity: Vector3
 onready var camera = $Target/Camera
 onready var gravity = -ProjectSettings.get_setting("physics/3d/default_gravity")
 onready var start_position = translation
+var isMoving:bool = false
 
 export (NodePath) var controllerPath
 onready var playerController : Controller = get_node(controllerPath)
@@ -18,15 +19,26 @@ onready var playerController : Controller = get_node(controllerPath)
 func _physics_process(_delta):
 	velocity.y += _delta * gravity
 	if playerController and playerController.is_working:
+		if isMoving:
+			get_node("AnimationPlayer2").play("Moving")
+			isMoving = false
 		velocity.x = playerController.output.x
 		velocity.z = playerController.output.y
 		velocity = move_and_slide(velocity * MAX_SPEED)
 		rotation = Vector3(0, playerController.output.angle()*-1, 0)
 		get_node("Root/BoosterFast").visible = true
+		get_node("Root/BoosterFast2").visible = true
 		get_node("Root/BoosterSlow").visible = false
+		get_node("Root/BoosterSlow2").visible = false
 	else:
+		if not isMoving:
+			get_node("AnimationPlayer2").play("NotMoving")
+			isMoving = true
+		
 		get_node("Root/BoosterFast").visible = false
+		get_node("Root/BoosterFast2").visible = false
 		get_node("Root/BoosterSlow").visible = true
+		get_node("Root/BoosterSlow2").visible = true
 	
 
 
