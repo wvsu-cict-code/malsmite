59b2932881381c029bb36b0383e8a707b419f760
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-06-24 17:22:15 +0800

    Twister animation

--- MalSmite/player/Player.gd
+++ MalSmite/player/Player.gd
@@ -21,31 +21,33 @@ func _physics_process(_delta):
 	if playerController and playerController.is_working:
 		if isMoving:
 			get_node("AnimationPlayer2").play("Moving")
+			get_node("AnimationPlayer3").play("Full Rotate")
 			isMoving = false
 		velocity.x = playerController.output.x
 		velocity.z = playerController.output.y
 		velocity = move_and_slide(velocity * MAX_SPEED)
 		rotation = Vector3(0, playerController.output.angle()*-1, 0)
-		get_node("Root/BoosterFast").visible = true
-		get_node("Root/BoosterFast2").visible = true
-		get_node("Root/BoosterSlow").visible = false
-		get_node("Root/BoosterSlow2").visible = false
-		get_node("Root/MotionTrail").visible = true
-		get_node("Root/MotionTrail2").visible = true
+		get_node("MainController/Root/BoosterFast").visible = true
+		get_node("MainController/Root/BoosterFast2").visible = true
+		get_node("MainController/Root/BoosterSlow").visible = false
+		get_node("MainController/Root/BoosterSlow2").visible = false
+		get_node("MainController/Root/MotionTrail").visible = true
+		get_node("MainController/Root/MotionTrail2").visible = true
 	else:
 		if not isMoving:
 			get_node("AnimationPlayer2").play("NotMoving")
+			get_node("AnimationPlayer3").stop()
 			isMoving = true
 		
-		get_node("Root/BoosterFast").visible = false
-		get_node("Root/BoosterFast2").visible = false
-		get_node("Root/BoosterSlow").visible = true
-		get_node("Root/BoosterSlow2").visible = true
-		get_node("Root/MotionTrail").visible = false
-		get_node("Root/MotionTrail2").visible = false
+		get_node("MainController/Root/BoosterFast").visible = false
+		get_node("MainController/Root/BoosterFast2").visible = false
+		get_node("MainController/Root/BoosterSlow").visible = true
+		get_node("MainController/Root/BoosterSlow2").visible = true
+		get_node("MainController/Root/MotionTrail").visible = false
+		get_node("MainController/Root/MotionTrail2").visible = false
 	
 
 
 func _on_FireButton_pressed():
-	get_node("Root/Weapon1").fire_weapon()
-	get_node("Root/Weapon2").fire_weapon()
+	get_node("MainController/Root/Weapon1").fire_weapon()
+	get_node("MainController/Root/Weapon2").fire_weapon()
