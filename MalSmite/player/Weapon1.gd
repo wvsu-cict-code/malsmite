9daa5f87f185c69e1c693d94c2bc9c895a877ff2
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-06-24 18:39:06 +0800

    Added sample virus

--- MalSmite/player/Weapon1.gd
+++ MalSmite/player/Weapon1.gd
@@ -9,7 +9,9 @@ func fire_weapon():
 	var clone = bullet_scene.instance()
 	var scene_root = get_tree().root.get_children()[0]
 	scene_root.add_child(clone)
+	get_node("AnimationPlayer").play("Flash")
 
 	clone.global_transform = self.global_transform
 	clone.scale = Vector3(4, 4, 4)
 	clone.BULLET_DAMAGE = DAMAGE
+	
